a490953bd1c7f6f1b3d3decbb79f5cb57724875c
Pedro Hurtado <pedrohurt@gmail.com>
2025-11-04 17:32:33 +0100

    mixins

---
+++ dia-2/arrays.js
@@ -0,0 +1,12 @@
+const a = [1,2,3,4,5,6]
+
+const result = a.filter(v=>v%2===0) //[2,4,6]
+a.find(v=>v%2===0) //2
+a.map(v=>v*v) [1,4,6,16,25,36]
+a.sort()
+a.filter(v=>v%2===0).map(v=>v*v).reduce((a,v)=>a+v,100)
+    //suma [1,4,6,16,25,36] + 100
+    //100+1*1=101
+    //101*1+2*2=a
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
---
+++ dia-2/ejercicio_mixin.js
@@ -0,0 +1,66 @@
+/*
+En mi sistema los clietes se pueden:
+    modificar
+    crear
+    remove
+    get
+Los usuario solo se pueden leer
+
+*/
+
+//Segregación interface(SOLID)
+
+const Add = Base => class extends Base {
+    add() { console.log("Add") }
+}
+const Update = Base => class extends Base {
+    update() { console.log("Update") }
+}
+const Remove = Base => class extends Base {
+    remove() { console.log("Remove") }
+}
+const Get = Base => class extends Base {
+    get() { console.log("get") }
+}
+
+/*class Customer extends Add(Update(Remove(Get(class {})))){
+    update(){
+        super.update();
+    }
+}
+class User extends(Get(class {})){
+
+}*/
+
+
+class Customer extends inherit().withMixins(Add, Update, Remove, Get) {
+
+}
+class User extends inherit().withMixins(Get) {
+}
+
+
+class Mixins {
+    constructor(Base) {
+        this.base = Base || class { };
+    }
+    withMixins(...mixins) {
+        return mixins.reduceRight((proto, mixin) => mixin(proto), this.base)
+        //Get,Remove,Update,Create
+        //base+Create
+        //base+Create+Upate
+        //base+Create+Upate+Remove
+        //base+Create+Upate+Remoe+Get
+        //null+Object+base+Create+Upate+Remoe+Get
+    }
+}
+
+function inherit(Base = class { }) {
+    return new Mixins(Base)
+}
+
+/*
+Demostramos que todos los datos de js heredean de object?
+1 es object si
+function es object si
+*/
